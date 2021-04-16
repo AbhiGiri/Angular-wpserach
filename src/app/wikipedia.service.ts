2c834acf928a6d90ec49c8d38ddaeb3f260994cf
AbhiGiri <abhishekmkumar003@gmail.com>
2021-04-16 16:58:51 +0530

    first commit

---
+++ src/app/wikipedia.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { pluck } from "rxjs/operators";
+
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      pageid: number;
+      snippet: string;
+      wordcount: number;
+    }[]
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WikipediaService {
+
+  constructor(private http: HttpClient) { }
+
+  public onSearch(term: string) {
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      }
+    }).pipe(pluck('query', 'search'));
+  }
+}
